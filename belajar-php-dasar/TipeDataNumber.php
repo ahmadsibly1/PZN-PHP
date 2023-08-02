6d421fc834f1c48722ebc1a475ab026e852c5241
ahmadsibly <60696334+ahmadsibly1@users.noreply.github.com>
2023-08-02 13:33:46 +0800

    :white_check_mark: Tipe data Number

---
+++ belajar-php-dasar/TipeDataNumber.php
@@ -0,0 +1,29 @@
+<?php
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1234);
+
+echo "Binary : ";
+var_dump(0b101010);
+
+echo "Underscore in number: ";
+var_dump(123_456);
+
+
+// float
+echo "Floating Point : ";
+var_dump(123.456);
+
+echo "Floating Point dengan E Notation: (1.7 x 1000) ";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E Notation Minus: (1.7 x 0.001) ";
+var_dump(1.7e-3);
+
+echo "Underscore di floating point: ";
+var_dump(123.456_789);
